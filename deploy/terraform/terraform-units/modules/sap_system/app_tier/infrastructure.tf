dd3735f168ad848805a78cef2d93b5f163a569fa
Kimmo Forss <kimmo.forss@microsoft.com>
2020-12-01 00:54:55 +0200

    Missed the app avset

--- deploy/terraform/terraform-units/modules/sap_system/app_tier/infrastructure.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/infrastructure.tf
@@ -142,7 +142,7 @@ resource "azurerm_availability_set" "app" {
   location                     = var.resource_group[0].location
   resource_group_name          = var.resource_group[0].name
   platform_update_domain_count = 20
-  platform_fault_domain_count  = 2
+  platform_fault_domain_count  = local.faultdomain_count
   proximity_placement_group_id = local.app_zonal_deployment ? var.ppg[count.index % local.app_zone_count].id : var.ppg[0].id
   managed                      = true
 }
