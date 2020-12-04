177a932df9998e173b08d5572254b1740c133cff
rukawata <rukawata@gmail.com>
2020-12-04 15:16:33 -0800

    Add local-exec for IMDS call on windows

--- deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/IMDS.tf
+++ deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/IMDS.tf
@@ -19,9 +19,16 @@ variable "max_timeout" {
 }
 
 // Registers the current deployment state with Azure's Metadata Service (IMDS)
-resource "null_resource" "IMDS" {
+resource "null_resource" "IMDS_linux" {
   provisioner "local-exec" {
     command    = "curl --silent --output /dev/null --max-time ${var.max_timeout} -i -H \"Metadata: \"true\"\" -H \"user-agent: SAP AutoDeploy/${var.auto-deploy-version}; scenario=${var.scenario}; deploy-status=Terraform_${var.scenario}\" http://169.254.169.254/metadata/instance?api-version=${var.api-version}"
+    on_failure = continue
+  }
+}
+
+resource "null_resource" "IMDS_windows" {
+  provisioner "local-exec" {
+    command    = "Invoke-RestMethod -Headers @{\"Metadata\"=\"true\"; \"user-agent\"=\"SAP AutoDeploy/${var.auto-deploy-version}\"; \"scenario\"=\"${var.scenario}_windows\"; \"deploy-status\"=\"Terraform_${var.scenario}\"} -Method GET -NoProxy -Uri http://169.254.169.254/metadata/instance?api-version=${var.api-version}"
     on_failure = continue
   }
 }
